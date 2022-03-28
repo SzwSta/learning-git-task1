5ff3175661e498908427ee758fd1cd8c1906fcb5
Stanislaw Szwiec <szwiec.st@jacobs.com>
2022-03-29 00:01:56 +0200

    add main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+zakupy = {
+    "Piekarnia": ["Chleb", "Paczek", "Bulki"],
+    "Warzywniak": ["Marchew", "Seler", "Rukola"]
+}
+print("Lista zakupow")
+a=0
+for i in zakupy:
+    print(f"Ide do {i}, kupuje tu nastepujace rzeczy: {zakupy[i]}")
+    a=a+len(zakupy[i])
+print(f"W sumie kupuje {a} elementow")
